45857acba73dcf7264a67a8d5cd413ea93aa5f4b
Daniel Rebollini <dar19@ic.ac.uk>
2019-10-17 13:27:52 +0400

    pendulum working

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,1 +1,21 @@
 #A program that simulates the pendulum equation
+
+import math
+
+def period(a, b):
+    x = isinstance(a, str)
+    y = isinstance(b, str)
+    if x == False:
+        if y == False:
+            if a < 0:
+                return(ValueError)
+            else:
+                if b < 0:
+                    return(ValueError)
+                else:
+                    c = 2 * math.pi * math.sqrt( a / b )
+                    return(c)
+        else:
+            return(TypeError)
+    else:
+        return(TypeError)
